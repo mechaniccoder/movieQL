a26b354247f05449ddaa70b640ada375262e0ea4
mechaniccoder <yuseunghwan94@gmail.com>
2020-07-09 13:03:57 +0900

    Add resolver function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { GraphQLServer } from "graphql-yoga";
-import resolvers from "../graphql/resolvers";
+import resolvers from "./graphql/resolvers";
 
 const server = new GraphQLServer({
-    typeDefs: "graphql/schema.graphql",
+    typeDefs: "src/graphql/schema.graphql",
     resolvers,
 });
 
